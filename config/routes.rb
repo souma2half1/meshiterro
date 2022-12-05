dc2a9c11541c2dcc2fdd0e8d79a729c6ed0a0163
souma2half1 <souma1441085@gmail.com>
2022-12-05 12:17:54 +0000

    12/5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
-  resources :post_images, only: [:new, :index, :show]
-  
+
+  resources :post_images, only: [:new, :create, :index, :show]
+
   devise_for :users
   root to: "homes#top"
-  
+
   get 'homes/about'=> 'homes#about',as: "about"
-  
+
 end
